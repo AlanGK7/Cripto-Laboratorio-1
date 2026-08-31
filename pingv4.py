831aaa2a73de73485a7e5ae0e86b5b701cfc0bf5
AlanGK7 <alan.sotomayor@mail.udp.cl>
2026-08-30 22:29:19 -0400

    fix payload

--- pingv4.py
+++ pingv4.py
@@ -17,13 +17,14 @@ secuencia = bytes(range(16,56))
 for caracter in palabra_incriptada:
     print(f"caracter: {caracter}")
     
-    tiempo_actual= time.time()
-    timestap_completo = struct.pack('d', tiempo_actual)  # Convertir el timestamp a bytes
-    resto = timestap_completo[1:8]  # Tomar los bytes del 1 al 7 (7 bytes)
+    tiempo_actual= int(time.time())
+    timestap_completo = struct.pack('<I', tiempo_actual)  # convertir el timestamp a bytes
+    resto = timestap_completo[1:3]  # tomar los bytes del 1 al 7 (7 bytes)
     
-    caracter = caracter.encode('utf-8')  # Convertir el carácter a bytes
+    caracter = caracter.encode('utf-8')  # convertir el carácter a bytes
+    ceros = b'\x00' * 5
     
-    palabra_48bytes = caracter + resto + secuencia
+    palabra_48bytes = caracter + resto + ceros + secuencia
     paquete = IP(dst="8.8.8.8") / solicitudICMP / palabra_48bytes
     respuesta = sr1(paquete, timeout=2)
     solicitudICMP.seq += 1  # Incrementar el número de secuencia para el siguiente paquete
